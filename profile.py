1b7736f104f586db5792bee21ff5c23a192faeae
Robert Hannah <roberthannah89@users.noreply.github.com>
2023-11-26 19:21:32 -0500

    Create profile.py

---
+++ profile.py
@@ -0,0 +1,87 @@
+"""
+Generate a good python profile that imports that 50 most common python modules with their standard abbreviations. 
+But split the profile into logical groupings of modules, and give a description with # at the end of the line that explains what each package does. 
+"""
+########################################
+# Python libraries
+########################################
+# Data Science and Numerical Computing
+import numpy as np                  # Fundamental package for scientific computing
+import pandas as pd                 # Data analysis and manipulation
+import scipy as sp                  # Scientific computing and technical computing
+import matplotlib.pyplot as plt     # Plotting library
+import seaborn as sns               # Statistical data visualization
+import statsmodels as sm            # Statistical modeling, hypothesis testing, and data exploration
+
+# Machine Learning
+import sklearn as skl               # Machine learning library
+import tensorflow as tf             # Deep learning and neural networks
+import keras                        # High-level neural networks API running on top of TensorFlow
+import torch                        # Deep learning library
+
+# System and File Operations
+import os                          # Interfacing with the operating system
+import sys                         # System-specific parameters and functions
+import glob                        # Unix style pathname pattern expansion
+
+# Date and Time
+import datetime as dt               # Basic date and time types
+import time                         # Time access and conversions
+
+# String and Regular Expression
+import re                          # Regular expression operations
+
+# Mathematics
+import math                         # Mathematical functions
+import random                       # Generate pseudo-random numbers
+import statistics                   # Mathematical statistics functions
+
+# Data Serialization and Configuration
+import json                         # JSON encoder and decoder
+import yaml                         # YAML parser and emitter
+import configparser                 # Configuration file parser
+
+# Networking and Internet
+import requests                     # HTTP library for sending requests and accessing responses
+import urllib3                      # HTTP client for Python
+import socket                       # Low-level networking interface
+import smtplib                      # SMTP protocol client
+import email                        # Managing email messages
+
+# Web Development
+import flask                        # Micro web framework
+import django                       # High-level Python web framework
+
+# Database
+import sqlite3                      # DB-API 2.0 interface for SQLite databases
+import sqlalchemy as sa             # SQL toolkit and Object-Relational Mapping (ORM) library
+import psycopg2                     # PostgreSQL database adapter
+
+# Image Processing
+import PIL.Image as Image           # Python Imaging Library fork
+import cv2                          # OpenCV library for computer vision tasks
+
+# File Compression and Archiving
+import zipfile                      # Work with ZIP archives
+import tarfile                      # Read and write tar archives
+
+# Data Input/Output
+import csv                          # CSV file reading and writing
+import io                           # Core tools for working with streams
+import xlrd                         # Read Excel files
+import openpyxl                     # Python library to read/write Excel 2010 xlsx/xlsm files
+
+# Utility and Miscellaneous
+import hashlib                      # Secure hash and message digest algorithms
+import logging                      # Logging facility for Python
+import argparse                     # Parser for command-line options, arguments
+import unittest                     # Unit testing framework
+import subprocess                   # Process creation and management
+import threading                    # Thread-based parallelism
+import multiprocessing              # Process-based parallelism
+import asyncio                      # Asynchronous I/O
+
+# HTML and XML Parsing
+import lxml                         # Processing XML and HTML
+import xml.etree.ElementTree as ET  # XML manipulation
+import bs4 as BeautifulSoup         # Web scraping
