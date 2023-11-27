6fefed041bcba1dc749ab2205bf760ac5dc90e43
Robert Hannah <roberthannah89@users.noreply.github.com>
2023-11-26 19:31:06 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,6 +1,9 @@
 """
+CHATGPT 4 PROMPT
 Generate a good python profile that imports that 50 most common python modules with their standard abbreviations. 
 But split the profile into logical groupings of modules, and give a description with # at the end of the line that explains what each package does. 
+However display this as a single code block containing many paragraphs. 
+Include the appropriate package when a package is deprecated. E.g. cv2.
 """
 ########################################
 # Python libraries
@@ -18,18 +21,21 @@ import sklearn as skl               # Machine learning library
 import tensorflow as tf             # Deep learning and neural networks
 import keras                        # High-level neural networks API running on top of TensorFlow
 import torch                        # Deep learning library
+import xgboost as xgb               # Optimized distributed gradient boosting library
 
 # System and File Operations
 import os                          # Interfacing with the operating system
 import sys                         # System-specific parameters and functions
 import glob                        # Unix style pathname pattern expansion
+import shutil                      # High-level file operations
 
 # Date and Time
 import datetime as dt               # Basic date and time types
 import time                         # Time access and conversions
 
-# String and Regular Expression
+# String and Regular Expressions
 import re                          # Regular expression operations
+import string                      # Common string operations
 
 # Mathematics
 import math                         # Mathematical functions
@@ -43,9 +49,9 @@ import configparser                 # Configuration file parser
 
 # Networking and Internet
 import requests                     # HTTP library for sending requests and accessing responses
-import urllib3                      # HTTP client for Python
+import urllib.request as urllib     # Module for working with URLs
 import socket                       # Low-level networking interface
-import smtplib                      # SMTP protocol client
+import smtplib                     # SMTP protocol client
 import email                        # Managing email messages
 
 # Web Development
@@ -57,9 +63,8 @@ import sqlite3                      # DB-API 2.0 interface for SQLite databases
 import sqlalchemy as sa             # SQL toolkit and Object-Relational Mapping (ORM) library
 import psycopg2                     # PostgreSQL database adapter
 
-# Image Processing
-import PIL.Image as Image           # Python Imaging Library fork
-import cv2                          # OpenCV library for computer vision tasks
+# Image Processing (Replacement for cv2)
+import PIL.Image as Image           # Python Imaging Library (Pillow) for image processing
 
 # File Compression and Archiving
 import zipfile                      # Work with ZIP archives
@@ -67,7 +72,6 @@ import tarfile                      # Read and write tar archives
 
 # Data Input/Output
 import csv                          # CSV file reading and writing
-import io                           # Core tools for working with streams
 import xlrd                         # Read Excel files
 import openpyxl                     # Python library to read/write Excel 2010 xlsx/xlsm files
 
@@ -85,3 +89,8 @@ import asyncio                      # Asynchronous I/O
 import lxml                         # Processing XML and HTML
 import xml.etree.ElementTree as ET  # XML manipulation
 import bs4 as BeautifulSoup         # Web scraping
+
+# Cloud and Distributed Computing
+import boto3                        # Amazon Web Services (AWS) SDK
+import apache_beam as beam          # Model and execute data processing workflows
+
